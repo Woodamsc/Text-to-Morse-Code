4b2169f32937d48bf2eb429d1d439a491abd54b7
Woodams Clark <WoodamsClark@gmail.com>
2017-02-12 15:58:10 -0500

    Python has a built in ArgParser, who knew!
    Learned python's argparser to replace my makeshift one.
    Added flags which can be seen with -h
    Flags (when in REPL mode) are toggle-able, so they stick until you call the flag again, or call the '--reset (-r)' flag.
    New flags allow you to:
    Go blind - No output at all is shown to you
    Turn off/on Morse - Control whether dits or dahs are shown to you
    Turn off/on Alphanumerics - Control whether the alphanumeric representation is displayed
    [ If you turn off Morse and Alpha, it's effectively Blind mode ]
    Restructured some googly bits and pieces.

--- morse.py
+++ morse.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python3
 import sys
+import argparse
+import shlex
 from subprocess import call
 from time import sleep
 
@@ -45,7 +47,7 @@ lookup = {
 "?":[	'.', 	'.',  '-', 	'-', 	'.',	'.'	],
 " " :[' | '],
 "\t":[' | '],
-"\n":[' | '],
+"\n":['\n'],
 }
 
 def pause():
@@ -60,18 +62,34 @@ def playSymbol( symbol ):
 	else:
 		return
 	call(["play", "-q", "-n", "synth", str( length ), "sin", "450"])
-	
+
+def printTranslation( translation ):
+# Determine when to print which characters. Also it plays a tone for the dits and dahs because I can't
+# think of a way to separate that out of these fruit-loops
+	global morse, blind, alpha
+	for tuple in translation:
+		m_code = tuple[1]
+		letter = tuple[0]
+
+		for symbol in m_code:
+			if not blind and morse:
+				print( symbol, end='', flush=True )
+			playSymbol( symbol )
+
+		if not blind and alpha and not (letter==" " or letter=="\t" or letter=="\n"):
+			print( '[' + letter + '] ', end='', flush=True )
+		pause()
+
+	if blind or morse or alpha:
+		print( "" )	
+
 def translate( input_text ):
+	translation = []
 	input_text = input_text.lower()
 	for letter in input_text:
 		if letter in lookup:
-			for symbol in lookup[letter]:
-				if readout:
-					print( symbol, end='', flush=True )
-				playSymbol( symbol )
-		pause()
-	if readout:
-		print( "" )
+			translation.append( [letter,lookup[letter]] )
+	return translation
 
 def tryAsFile(input):
 # Just to make things more readable
@@ -82,26 +100,51 @@ def tryAsFile(input):
 		pass
 	return input
 
-#### E N T R Y ####
-readout = True
-num_args = len( sys.argv )
-if str( sys.argv[num_args-1] ) == '--noreadout':
-	readout = False
-	num_args -= 1
+def updateArgs():
+# Flags can be toggled on and off by entering the same flag in again (for use in REPL). Not super kosher.
+	global alpha, blind, morse
+	if args.reset:
+		alpha = False
+		blind = False
+		morse = True
+		return
 
-if num_args > 1:
-	text = str( sys.argv[1] )
-	text = tryAsFile( text )
-	translate( text )
+	if args.alpha:
+		alpha = ~alpha
+
+	if args.blind:
+		blind = ~blind
+
+	if args.no_morse:
+		if morse:	morse = False
+		else:			morse = True
+
+#### E N T R Y ####
+alpha = False
+blind = False
+morse = True
+parser = argparse.ArgumentParser()
+parser.add_argument( "text", nargs="?", 	help="Text or File to be translated, quotes are okay" )
+parser.add_argument( "-b", "--blind", 		help="Go Blind; disable printing out any text", action="store_true" )
+parser.add_argument( "-nm", "--no_morse", help="Disable printing out morse code", 			action="store_true" )
+parser.add_argument( "-a", "--alpha", 		help="Print alphanumeric text",						action="store_true" )
+parser.add_argument( "-r", "--reset", 		help="Reset all arguments",							action="store_true" )
+args = parser.parse_args()
+updateArgs()
+
+if args.text != None:
+	text = tryAsFile( args.text )
+	printTranslation( translate( text ) )
 
 else:
 	while(True):
 		try:
-			text = input( "> " )
-			if text == '--noreadout':
-				readout = ~readout
-			else:
-				translate( text )
+			input_text = input( "> " )
+			args = parser.parse_args( shlex.split( input_text ) )
+			updateArgs()
+			if args.text != None:
+				text = tryAsFile( args.text )
+				printTranslation( translate( text ) )
 
 		except EOFError:
 			print( "-...-.--." )
